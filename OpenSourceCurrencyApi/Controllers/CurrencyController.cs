33e7b1c64f73ba72df8be850cba3c5755ece5565
Shaswata Tripathy <tripathyshaswata@gmail.com>
2021-12-05 23:38:14 +0530

    changes for csv

--- OpenSourceCurrencyApi/Controllers/CurrencyController.cs
+++ OpenSourceCurrencyApi/Controllers/CurrencyController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using OpenSourceCurrencyApi.Client;
+using OpenSourceCurrencyApi.Models;
 using OpenSourceCurrencyApi.Repository;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace OpenSourceCurrencyApi.Controllers
 {
@@ -32,6 +36,30 @@ namespace OpenSourceCurrencyApi.Controllers
             var result = await _currencyRepository.GetAllCurrencies();
 
             return Ok(result);
+        }
+
+        [HttpGet("comparison/download/{currency}")]
+        public async Task<FileResult> DownloadComparisonResult(string currency)
+        {
+            CurrencyComparator result = await _currencyRepository.GetCurrencyComparisonResult(currency);
+            
+            string fileName = $"{result.Date}_{currency}";
+            
+
+            
+            IEnumerable<CurrencyPrice> currencyPriceList = result.CurrencyBasePriceList;
+
+            var properties = typeof(CurrencyPrice).GetProperties().Select(x=> x.Name).ToList();
+            var headers = string.Join(",", properties);
+
+            var sb = new StringBuilder();
+            sb.AppendLine(headers);
+            foreach (var data in currencyPriceList)
+            {
+                sb.AppendLine(data.CurrencyShortName + "," + data.ExchangePrice);
+            }
+
+            return File(new UTF8Encoding().GetBytes(sb.ToString()), "text/csv", fileName);
         }
     }
 }
